bcbb525afa4dcb6f545825b0984cfbb054c28954
Bekarys7 <bekarys.ergazin7@gmail.com>
2025-12-09 11:34:34 +0500

    hotifx

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,8 @@
 import { RouterProvider } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { useAppDispatch } from "./store/hooks/hooks";
-import { checkAuth } from "./store/authActions";
-import { App as AntdApp, notification } from "antd";
-import LoadingSpinner from "./components/app/LoadingSpinner";
+import { App as AntdApp } from "antd";
 import router from "./router";
 
 function App() {
-  const dispatch = useAppDispatch();
-  const [isInitialized, setIsInitialized] = useState<boolean>(false);
-
-  useEffect(() => {
-    const initAuth = async () => {
-      const refreshToken = localStorage.getItem("refreshToken");
-
-      if (refreshToken) {
-        try {
-          await dispatch(checkAuth()).unwrap();
-        } catch (error) {
-          notification.error({
-            message: "Auth check failed",
-            description: `${error}`,
-          });
-        }
-      }
-
-      setIsInitialized(true);
-    };
-
-    initAuth();
-  }, [dispatch]);
-
-  if (!isInitialized) {
-    return <LoadingSpinner />;
-  }
-
   return (
     <AntdApp>
       <RouterProvider router={router} />
--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,36 +1,71 @@
-import { Outlet } from "react-router-dom";
-import { Card, Col, Row } from "antd";
+import { Navigate, Outlet } from "react-router-dom";
+import { Card, Col, notification, Row } from "antd";
 import styles from "../pages/auth/AuthPage.module.scss";
 import authBackground from "../assets/authBackground.svg";
+import LoadingSpinner from "../components/app/LoadingSpinner";
+import { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../store/hooks/hooks";
+import { checkAuth } from "../store/authActions";
 
-function AuthLayout() {
+const AuthLayout: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const isAuth = useAppSelector((state) => state.auth.isAuth);
+  const [isInitialized, setIsInitialized] = useState<boolean>(false);
+
+  useEffect(() => {
+    const initAuth = async () => {
+      const refreshToken = localStorage.getItem("refreshToken");
+
+      if (refreshToken) {
+        try {
+          await dispatch(checkAuth()).unwrap();
+        } catch (error) {
+          notification.error({
+            message: "Auth check failed",
+            description: `${error}`,
+          });
+        }
+      }
+
+      setIsInitialized(true);
+    };
+
+    initAuth();
+  }, [dispatch]);
+
+  if (isAuth) {
+    return <Navigate to="/tasks" replace />;
+  }
+
+  if (!isInitialized) {
+    return <LoadingSpinner />;
+  }
+
   return (
-    <>
-      <div className={styles.authWrapper}>
-        <Card
-          hoverable
-          className={styles.cardStyle}
-          styles={{
-            body: { padding: 0 },
-          }}
-        >
-          <Row align="middle" style={{ cursor: "default" }}>
-            <Col xs={24} md={15}>
-              <img
-                draggable={false}
-                alt="auth-background"
-                src={authBackground}
-                className={styles.imgStyle}
-              />
-            </Col>
+    <div className={styles.authWrapper}>
+      <Card
+        hoverable
+        className={styles.cardStyle}
+        styles={{
+          body: { padding: 0 },
+        }}
+      >
+        <Row align="middle" style={{ cursor: "default" }}>
+          <Col xs={24} md={15}>
+            <img
+              draggable={false}
+              alt="auth-background"
+              src={authBackground}
+              className={styles.imgStyle}
+            />
+          </Col>
 
-            <Col xs={24} md={8}>
-              <Outlet />
-            </Col>
-          </Row>
-        </Card>
-      </div>
-    </>
+          <Col xs={24} md={8}>
+            <Outlet />
+          </Col>
+        </Row>
+      </Card>
+    </div>
   );
-}
+};
 export default AuthLayout;
