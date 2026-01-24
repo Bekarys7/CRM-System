20017270a3e1e2a2b52e3da9a727724e93718e3c
Bekarys7 <bekarys.ergazin7@gmail.com>
2026-01-24 22:47:11 +0500

    change

--- src/pages/auth/AuthPage.tsx
+++ src/pages/auth/AuthPage.tsx
@@ -2,15 +2,9 @@ import React from "react";
 import { useAppSelector } from "../../store/hooks/hooks";
 import { Navigate } from "react-router-dom";
 import LoginForm from "../../components/auth/LoginForm";
-import LoadingSpinner from "../../components/app/LoadingSpinner";
 
 const AuthPage: React.FC = () => {
   const isAuth = useAppSelector((state) => state.auth.isAuth);
-  const status = useAppSelector((state) => state.auth.status);
-
-  if (status === "pending") {
-    return <LoadingSpinner />;
-  }
 
   if (isAuth) {
     return <Navigate to="/tasks" replace />;
